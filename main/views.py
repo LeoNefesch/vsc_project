a0230e4ec2721eb7189fe483a29b5e202391d8da
leonid.negaschev <leonid.negaschev@gmail.com>
2022-05-09 13:35:54 +0500

    add data to main/views.py

--- main/views.py
+++ main/views.py
@@ -1,8 +1,19 @@
+from array import array
+
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', 'some string'],
+        'obj': {
+            'car': 'BMW',
+            'age': 25,
+            'hobby': 'guitar'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
