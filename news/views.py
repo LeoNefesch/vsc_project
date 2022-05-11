a42b391a81849051420d6496fbd1212ba77cf85f
leonid.negaschev <leonid.negaschev@gmail.com>
2022-05-11 12:16:20 +0500

    Refactor

--- news/views.py
+++ news/views.py
@@ -8,7 +8,7 @@ from .models import Articles
 
 
 def news_home(request):
-    news = Articles.objects.order_by('date')
+    news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
 
 
