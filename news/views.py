4b9103b8db465fc8334f4e1d2ba3f760053481f4
leonid.negaschev <leonid.negaschev@gmail.com>
2022-05-12 11:51:12 +0500

    Add two new classes

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from re import template
 
 from django.shortcuts import redirect, render
-from django.views.generic import DetailView
+from django.views.generic import DeleteView, DetailView, UpdateView
 
 from .forms import ArticlesForm
 from .models import Articles
@@ -16,6 +16,18 @@ class NewsDetailView(DetailView):
     model = Articles
     template_name = 'news/details_view.html'
     context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
 
 
 def create(request):
